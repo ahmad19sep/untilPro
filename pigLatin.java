81003689399c49599747d41280dfe0ec663ec3ec
ahmad19sep <ahmadsiddiqueofficial1820665@gmail.com>
2023-11-20 12:10:19 +0500

    change the sentence

--- pigLatin.java
+++ pigLatin.java
@@ -1,5 +1,14 @@
 public class pigLatin {
     public static void main(String[] args) {
-        System.out.println("Hello");
+       String sentence="This is an sentence";
+       String[] token=sentence.split(" ");
+       char ch='q';
+       sentence="";
+        for (String s : token) {
+            ch = s.charAt(0);
+            sentence = sentence.concat(s.substring(1) + ch + "ay  ");
+
+        }
+        System.out.println(sentence);
     }
 }
